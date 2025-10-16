339e9f6288680af7a80a6db990a4b1d7e4756b36
TNS INDIA <TNS INDIA@LAPTOP-N0JHV8G0>
2025-10-16 16:28:53 +0530

    first

---
+++ Corejavaprograms/src/com/tnsif/accessmodifierdemo/Accessdemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.accessmodifierdemo;
+// demo for access modifier
+
+public class Accessdemo {
+	
+	// public 
+	 int a=9;
+	String s="guru";
+	
+	public void display() {
+		System.out.println("welcome to java");
+	}
+	
+	public static void main(String[] args) {
+		Accessdemo obj=new Accessdemo();
+		System.out.println(obj.a);
+		System.out.println(obj.s);
+		
+		obj.display();
+	}
+
+}
--- Corejavaprograms/src/com/tnsif/program/Demoprogram.java
+++ Corejavaprograms/src/com/tnsif/program/Demoprogram.java
@@ -1,6 +1,7 @@
 package com.tnsif.program;
 
 public class Demoprogram {
+	
 public static void main(String[] args) {
 	System.out.println("welcome to vkit");
 }
