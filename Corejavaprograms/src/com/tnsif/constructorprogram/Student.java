e7e52a5f9da8cea4ebdabded2dd13c7a8b721184
TNS INDIA <TNS INDIA@LAPTOP-N0JHV8G0>
2025-10-23 16:04:43 +0530

    first

---
+++ Corejavaprograms/src/com/tnsif/constructorprogram/Student.java
@@ -0,0 +1,12 @@
+package com.tnsif.constructorprogram;
+// this key word
+public class Student {
+int rollno;
+String name;
+
+// parameter constructor 
+public Student(int rollno, String name) {
+	this.rollno = rollno;
+	this.name = name;
+}
+}
